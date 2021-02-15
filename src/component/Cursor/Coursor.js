2cb3178ed4ac114431d3c60d26042d85f4022e2e
Islamshaban98 <islamshaba998@gmail.com>
2021-02-15 13:06:47 +0200

    create custom cursor

--- src/component/Cursor/Coursor.js
+++ src/component/Cursor/Coursor.js
@@ -1,5 +1,55 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+// import Div from "./Cursor.style";
+import classNames from "classnames";
+import "./cursor.css";
 export const Coursor = () => {
-  return <div />;
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [linkHovered, setLinkHovered] = useState(false);
+  const [hidden, setHidden] = useState(false);
+  useEffect(() => {
+    addEventListeners();
+    handleLinkHoverEvents();
+    return () => removeEventListeners();
+  }, []);
+
+  const addEventListeners = () => {
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseenter", onMouseEnter);
+    document.addEventListener("mouseleave", onMouseLeave);
+  };
+  const onMouseLeave = () => {
+    setHidden(true);
+  };
+
+  const onMouseEnter = () => {
+    setHidden(false);
+  };
+  const removeEventListeners = () => {
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseenter", onMouseEnter);
+    document.removeEventListener("mouseleave", onMouseLeave);
+  };
+  const handleLinkHoverEvents = () => {
+    document.querySelectorAll("a").forEach((el) => {
+      el.addEventListener("mouseover", () => setLinkHovered(true));
+      el.addEventListener("mouseout", () => setLinkHovered(false));
+    });
+  };
+  const onMouseMove = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+  const cursorHover = classNames("cursor", {
+    "cursor--hidden": hidden,
+    "cursor--link-hovered": linkHovered,
+  });
+
+  return (
+    <div
+      className={cursorHover}
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    />
+  );
 };
